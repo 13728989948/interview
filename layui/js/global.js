be72125d63288ae31bacbcb1f86614d79fc8aefd
ice <ice@ice-PC>
2017-04-19 12:12:51 +0800

    Signed-off-by: ice <ice@ice-PC>

--- layui/js/global.js
+++ layui/js/global.js
@@ -1,18 +1,4 @@
 layui.define([ "layer", "code", "form", "element", "util" ], function(exports) {
 	var layer = layui.layer, form = layui.form(), util = layui.util, device = layui.device();
-
-	debugger;
-	
-	// 手机设备的简单适配
-	var shadeMobile = $(".site-mobile-shade");
-
-	$("#mbx_image").on("click", function() {
-		$("body").addClass("site-mobile");
-	});
-
-	shadeMobile.on("click", function() {
-		$("body").removeClass("site-mobile");
-	});
-
 	exports("global", {});
 });
